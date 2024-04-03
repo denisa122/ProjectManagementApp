7f83bf6758edde57077da6d4f8d60597c389efda
denisa122 <neagudenisa12@yahoo.com>
2024-04-03 18:03:56 +0200

    connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,24 @@ const app = express();
 
 require('dotenv-flow').config();
 
+// Routes
+app.get("/api/welcome", (req, res) => {
+    res.status(200).send("Welcome to the Express API!");
+});
+
+mongoose.connect
+(
+    process.env.DBHOST,
+    {
+
+    }
+).catch(error => console.log("Error connecting to MongoDB: ", error));
+
+mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+
 const PORT = process.env.PORT || 4000;
 
+// Start up the server
 app.listen(PORT, function() {
     console.log('Server is running on Port:', PORT);
 })
