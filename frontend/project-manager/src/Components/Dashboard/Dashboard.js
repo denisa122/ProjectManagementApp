1ea7b5859b767e9960ee60ea40ee6ccb2427c51c
denisa122 <neagudenisa12@yahoo.com>
2024-05-16 13:38:25 +0200

    delete project implemented

--- frontend/project-manager/src/Components/Dashboard/Dashboard.js
+++ frontend/project-manager/src/Components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 import "./Dashboard.css";
@@ -13,12 +13,13 @@ import Logo from "../../assets/logo.png";
 import Plus from "../../assets/plus.svg";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const [isTeamLeader, setIsTeamLeader] = useState(false);
   const [userId, setUserId] = useState(null);
-
   const [projects, setProjects] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [projectIdToDelete, setProjectIdToDelete] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +29,6 @@ const Dashboard = () => {
           return;
         }
 
-        // Fetch user role and ID
         const roleResponse = await axios.get(
           "http://localhost:5000/api/user/login-status",
           {
@@ -42,7 +42,6 @@ const Dashboard = () => {
         const id = roleResponse.data?.id;
         setUserId(id);
 
-        // Fetch projects
         if (id) {
           const projectsResponse = await axios.get(
             `http://localhost:5000/api/projects/users/${id}`,
@@ -65,12 +64,38 @@ const Dashboard = () => {
     fetchData();
   }, []);
 
+  const handleConfirmDelete = async () => {
+    try {
+      const token = localStorage.getItem("token");
+      await axios.delete(
+        `http://localhost:5000/api/projects/${projectIdToDelete}`,
+        {
+          headers: {
+            "auth-token": token,
+          },
+        }
+      );
+
+      setProjects((prevProjects) =>
+        prevProjects.filter((project) => project._id !== projectIdToDelete)
+      );
+
+      setProjectIdToDelete(null);
+    } catch (error) {
+      console.error("Error deleting project:", error);
+    }
+  };
+
+  const handleCancelDelete = () => {
+    setProjectIdToDelete(null);
+  };
+
   return (
     <div>
       <Navigation />
       <div className="dashboardContainer">
         <div className="dashboardHeader">
-          <h1>{isTeamLeader ? "Your Team's Project" : "Team Projects"}</h1>
+          <h1>{isTeamLeader ? "Your Team's Projects" : "Team Projects"}</h1>
           <img src={Logo} alt="logo" className="dashboardLogo" />
         </div>
         <div>
@@ -102,16 +127,36 @@ const Dashboard = () => {
                   startDate={project.startDate}
                   endDate={project.endDate}
                 />
-                <Link to={`/projects/${project._id}`} className="detailsButton">
+                <Link
+                  to={`/projects/${project._id}`}
+                  className="detailsButton"
+                >
                   See Details
                 </Link>
+                {isTeamLeader && (
+                  <button
+                    onClick={() => setProjectIdToDelete(project._id)}
+                    className="deleteButton"
+                  >
+                    Delete Project
+                  </button>
+                )}
               </div>
             ))
           )}
         </div>
-
-        <Footer />
       </div>
+      {projectIdToDelete && (
+        <div className="confirmationContainer">
+          <div className="confirmationBox">
+            <p>Are you sure you want to delete this project?</p>
+            <div>
+              <button onClick={handleConfirmDelete}>Yes</button>
+              <button onClick={handleCancelDelete}>No</button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
