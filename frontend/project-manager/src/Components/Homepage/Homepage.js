a7ac528b5c4f1b0e4e0a8291945f6de131a49ebc
denisa122 <neagudenisa12@yahoo.com>
2024-05-16 17:44:09 +0200

    tailwind installed + homepage restyled

--- frontend/project-manager/src/Components/Homepage/Homepage.js
+++ frontend/project-manager/src/Components/Homepage/Homepage.js
@@ -48,51 +48,125 @@ const Homepage = () => {
 
   return (
     <div>
-      <div className="homepageContainer">
-        <div className="heroImgContainer">
-          <img src={Logo} alt="Logo" className="homepageLogo" />
-          <h1 className="homepageTitle">
-            Bringing teams together. <br></br> One task at a time, <br></br>{" "}
-            towards common success.
-          </h1>
-        </div>
-        <div className="homepageLoginFormContainer">
-          <h2>
-            <strong>Jump back in</strong>
-          </h2>
-          <p>
-            Your teammates need you. <br></br> Log in to get started.
-          </p>
-          <form id="homepageLoginForm" onSubmit={handleSubmit}>
-            <label htmlFor="email">Work email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
+      <section class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700"
+      style={{display: "flex", justifyContent: "center", alignItems: "center", textAlign: "center", minHeight: "100vh"}}>
+        <div class="container h-full p-10">
+          <div class="flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+            <div class="w-full">
+              <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                <div class="g-0 lg:flex lg:flex-wrap">
+                  {/* <!-- Left column container--> */}
+                  <div class="px-4 md:px-0 lg:w-6/12">
+                    <div class="md:mx-6 md:p-12">
+                      {/* <!--Logo--> */}
+                      <div class="text-center">
+                        <img class="mx-auto w-32" src={Logo} alt="logo" />
+                        <h4 class="mb-2 mt-8 pb-1 text-xl font-semibold">
+                          Jump back in
+                        </h4>
+                      </div>
 
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
+                      <form id="homepageLoginForm" onSubmit={handleSubmit}>
+                        <h3 class="mb-10 text-lg">
+                          Your teammates need you. Log in to get started.
+                        </h3>
+                        {/* <!--Work email input--> */}
+                        <div class="relative mb-4" data-twe-input-wrapper-init>
+                          <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                            placeholder="Work email"
+                          />
+                          <label
+                            htmlFor="email"
+                            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+                          >
+                            Work email
+                          </label>
+                        </div>
 
-            <button type="submit">Log in</button>
-          </form>
+                        {/* <!--Password input--> */}
+                        <div class="relative mb-4" data-twe-input-wrapper-init>
+                          <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            class="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                          />
+                          <label
+                            htmlFor="password"
+                            class="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
+                          >
+                            Password
+                          </label>
+                        </div>
 
-          <div>
-            <p className="homepageSignupSection">
-              Don't have an account? <Link to="/register">Sign up</Link>
-            </p>
+                        {/* <!--Submit button--> */}
+                        <div class="mb-12 pb-1 pt-1 text-center">
+                          <button
+                            class="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
+                            type="submit"
+                            data-twe-ripple-init
+                            data-twe-ripple-color="light"
+                            style={{
+                              background:
+                                "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                            }}
+                          >
+                            Log in
+                          </button>
+
+                          {/* Forgot password link */}
+                          <a href="#!">Forgot password?</a>
+                        </div>
+
+                        {/* <!--Register button--> */}
+                        <div class="flex items-center justify-between pb-6">
+                          <p class="mb-0 me-2">Don't have an account?</p>
+                          <Link to="/register">
+                            <button
+                              type="button"
+                              class="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-danger-50/50 hover:text-danger-600 focus:border-danger-600 focus:bg-danger-50/50 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-rose-950 dark:focus:bg-rose-950"
+                              data-twe-ripple-init
+                              data-twe-ripple-color="light"
+                            >
+                              Register
+                            </button>
+                          </Link>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+
+                  {/* <!-- Right column container with background and description--> */}
+                  <div
+                    class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-e-lg lg:rounded-bl-none"
+                    style={{
+                      background:
+                        "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
+                    }}
+                  >
+                    <div class="px-4 py-6 text-white md:mx-6 md:p-12">
+                      <h1 class="mb-6 font-semibold"
+                      style={{fontSize: "2.25rem", lineHeight: "2.75rem", textAlign: "left"}}>
+                        Bringing teams together. <br></br>
+                        One task at a time, <br></br>
+                        towards common success.
+                      </h1>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      <Footer />
+      </section>
     </div>
   );
 };
