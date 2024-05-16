24bc584c24f6097873ee0ca3927ce9a6d3d7b127
denisa122 <neagudenisa12@yahoo.com>
2024-05-16 19:32:55 +0200

    navigation restyled

--- frontend/project-manager/src/Components/Navigation/Navigation.js
+++ frontend/project-manager/src/Components/Navigation/Navigation.js
@@ -3,10 +3,10 @@ import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 
-import "./Navigation.css";
-
-import NotificationIcon from "../../assets/notification.svg";
+import Logo from "../../assets/logo.png";
 import ProfileIcon from "../../assets/avatar.svg";
+
+import "./Navigation.css";
 
 const Navigation = () => {
   const navigate = useNavigate();
@@ -53,28 +53,105 @@ const Navigation = () => {
   };
 
   return (
-    <nav className="navbar">
-      <div className="leftSide">
-        <Link to="/dashboard">Projects</Link>
-        {isTeamLeader && (
-          <Link to="/team-details" style={{ paddingLeft: "20px" }}>
-            Team
-          </Link>
-        )}
-      </div>
-      <div className="rightSide">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="searchbar"
-        ></input>
-        <img
-          src={NotificationIcon}
-          alt="notification icon"
-          className="icon"
-        ></img>
-        <img src={ProfileIcon} alt="profile icon" className="avatar"></img>
-        <button onClick={handleLogout}>Log out</button>
+    <nav className="bg-gradient border-gradient relative flex w-full flex-nowrap items-center justify-between bg-sky-100 py-2 shadow-dark-mild lg:flex-wrap lg:justify-start lg:py-4">
+      <div className="flex w-full flex-wrap items-center justify-between px-3">
+        <div
+          className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
+          id="navbarSupportedContent1"
+          data-twe-collapse-item
+        >
+          {/** Logo */}
+          {/* <a
+            className="mb-4 me-5 ms-2 mt-3 flex items-center text-neutral-900 hover:text-neutral-900 focus:text-neutral-900 dark:text-neutral-200 dark:hover:text-neutral-400 dark:focus:text-neutral-400 lg:mb-0 lg:mt-0"
+            href="#"
+          >
+            <img
+              src={Logo}
+              style={{ height: "40px" }}
+              alt="CollabHub Logo"
+              loading="lazy"
+            />
+          </a> */}
+
+          {/** Left navigation links */}
+          <ul
+            className="list-style-none me-auto flex flex-col ps-0 lg:flex-row"
+            data-twe-navbar-nav-ref
+          >
+            <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+              {/** Dashboard link */}
+              <Link
+                className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                style={{fontSize: "large", fontWeight: "400"}}
+                to="/dashboard"
+                data-twe-nav-link-ref
+              >
+                Dashboard
+              </Link>
+            </li>
+            {/** Team link */}
+            <li className="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
+              {isTeamLeader && (
+                <Link
+                  className="text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
+                  style={{fontSize: "large", fontWeight: "400"}}
+                  to="/team-details"
+                  data-twe-nav-link-ref
+                >
+                  Team
+                </Link>
+              )}
+            </li>
+          </ul>
+          <div className="relative flex items-center">
+            <a className="me-4 text-neutral-600 dark:text-white" href="#">
+              <span className="[&>svg]:w-5">
+                <img
+                  src={ProfileIcon}
+                  style={{ height: "35px" }}
+                  alt="profile icon"
+                  loading="lazy"
+                />
+              </span>
+            </a>
+
+            <a
+              className="me-4 flex items-center text-neutral-600 dark:text-white relative"
+              href="#"
+              id="dropdownMenuButton1"
+              role="button"
+              data-twe-dropdown-toggle-ref
+              aria-expanded="false"
+            >
+              {/** Dropdown trigger icon */}
+              <span className="w-8 h-8">
+                {" "}
+                {/* Adjust width and height as needed */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-full h-full"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M5.25 9a6.75 6.75 0 0113.5 0v.75c0 2.123.8 4.057 2.118 5.52a.75.75 0 01-.297 1.206c-1.544.57-3.16.99-4.831 1.243a3.75 3.75 0 11-7.48 0 24.585 24.585 0 01-4.831-1.244.75.75 0 01-.298-1.205A8.217 8.217 0 005.25 9.75V9zm4.502 8.9a2.25 2.25 0 104.496 0 25.057 25.057 0 01-4.496 0z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+              </span>
+              {/** Notification counter */}
+              <span className="absolute -top-1 -right-1 rounded-full bg-danger px-[0.35em] py-[0.15em] text-[0.6rem] font-bold leading-none text-white">
+                1
+              </span>
+            </a>
+
+            <a className="me-4 flex items-center text-neutral-600 dark:text-white text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2">
+              <button onClick={handleLogout}
+              style={{fontSize: "large", fontWeight: "400"}}>Log out</button>
+            </a>
+          </div>
+        </div>
       </div>
     </nav>
   );
