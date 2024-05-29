0765d1bb8ecdfb1a67a93eb64928b10ae6dd955d
denisa122 <neagudenisa12@yahoo.com>
2024-05-29 15:29:57 +0200

    auth logout + test login status tests implemented

--- test/user.test.js
+++ test/user.test.js
@@ -100,4 +100,80 @@ describe('Register & login tests', () => {
                 done();
             });
     });
+
+    it('should log in and then log out a user', (done) => {
+        // Login the user
+        chai.request(server)
+        .post('/api/user/login')
+        .send({
+            email: registeredUser.email,
+            password: registeredUser.password
+        })
+        .end((err, res) => {
+            expect(res.status).to.be.equal(200);
+            res.body.should.have.property('data');
+            res.body.data.should.have.property('token');
+
+            let token = res.body.data.token;
+
+            // Logout the user
+            chai.request(server)
+            .post('/api/user/logout')
+            .set('auth-token', token)
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.message).to.be.equal('Logged out successfully');
+
+                done();
+            });
+        });
+    });
+});
+
+describe('Login status tests', () => {
+    it('should return isLoggedIn false when no token is provided', (done) => {
+        chai.request(server)
+        .get('/api/user/login-status')
+        .end((err, res) => {
+            expect(res.status).to.be.equal(200);
+            expect(res.body).to.be.a('object');
+            expect(res.body.isLoggedIn).to.be.false;
+
+            done();
+        });
+    });
+
+    it('should return isLoggedIn true when a valid token is provided', (done) => {
+        // Login the user
+        chai.request(server)
+        .post('/api/user/login')
+        .send({
+            email: 'paulsmith@email.com',
+            password: 'password'
+        })
+        .end((err, res) => {
+            expect(res.status).to.be.equal(200);
+            res.body.should.have.property('data');
+            res.body.data.should.have.property('token');
+
+            let token = res.body.data.token;
+
+            // Check login status
+            chai.request(server)
+            .get('/api/user/login-status')
+            .set('auth-token', token)
+            .end((err, res) => {
+                expect(res.status).to.be.equal(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.isLoggedIn).to.be.true;
+                expect(res.body).to.have.property('role');
+                expect(res.body.role).to.be.equal('team member');
+                expect(res.body).to.have.property('id');
+                expect(res.body.id).to.be.a('string');
+
+                done();
+            });
+        });
+    });
 });
